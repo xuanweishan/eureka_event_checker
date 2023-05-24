777d07b6d2c3bb16d2211312a3d4f982df745f4a
xuanweishan <xuanweishan@gmail.com>
2023-05-24 14:09:38 +0800

    Start project and create main script with usage information.

---
+++ event_checker.py
@@ -0,0 +1,14 @@
+"""
+Usage: event_checker.py nodes_state_now nodes_state_early
+
+Description:
+Check node event such as:
+1. Node down
+2. CPU high temperature
+3. GPU high temperature
+4. InfiniBand adaptor high temperature
+5. InfiniBand Speed not correct
+6. Disk volume high usage
+7. Non-pbs user occur in computing nodes
+
+"""
