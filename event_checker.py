c790173663b2351b2759905a7476be12428ae6aa
xuanweishan <xuanweishan@gmail.com>
2023-06-13 17:39:50 +0800

    Finish the event checker.

--- event_checker.py
+++ event_checker.py
@@ -14,6 +14,7 @@ Check node event such as:
 """
 import sys
 import os
+import time
 
 sys.dont_write_bytecode = True
 
@@ -73,14 +74,15 @@ def load_state(file_name):
     return node_state
 
 def check_node_down(current_state, previous_state):
-    new_down = []
+    new_down = {}
     for node in current_state:
         if not current_state[node]['State']  == 'Down':
             continue
         elif previous_state[node]['State'] == 'Down':
             continue
         else:
-            new_down.append(node)
+            new_down[node] = {'Job': previous_state[node]['Job']}
+
     return new_down
 
 def check_CPU_temp(node_state, Temp_limit):
@@ -89,7 +91,7 @@ def check_CPU_temp(node_state, Temp_limit):
         if node_state[node]['State'] == 'Down':
             continue
         if float(node_state[node]['T_CPU']) > Temp_limit:
-            high_temp_nodes[node] = node_state[node]['T_CPU']
+            high_temp_nodes[node] = {'T_CPU': node_state[node]['T_CPU'], 'Job': node_state[node]['Job']}
 
     return high_temp_nodes
 
@@ -99,7 +101,7 @@ def check_GPU_temp(node_state, Temp_limit):
         if node_state[node]['State'] == 'Down':
             continue
         if float(node_state[node]['T_GPU']) > Temp_limit:
-            high_temp_nodes[node] = node_state[node]['T_GPU']
+            high_temp_nodes[node] = {'T_GPU': node_state[node]['T_GPU'], 'Job': node_state[node]['Job']}
 
     return high_temp_nodes
     
@@ -109,7 +111,7 @@ def check_IB_temp(node_state, IB_temp_limit):
         if node_state[node]['State'] == 'Down':
             continue
         if float(node_state[node]['T_IB']) > IB_temp_limit:
-            high_temp_nodes[node] = node_state[node]['T_IB']
+            high_temp_nodes[node] = {'T_IB': node_state[node]['T_IB'], 'Job': node_state[node]['Job']}
 
     return high_temp_nodes
 
@@ -119,7 +121,7 @@ def check_IB_speed(node_state, IB_speed_threshold):
         if node_state[node]['State'] == 'Down':
             continue
         if float(node_state[node]['IB_speed']) < IB_speed_threshold:
-            low_speed_nodes[node] = node_state[node]['IB_speed']
+            low_speed_nodes[node] = {'IB_speed':node_state[node]['IB_speed'], 'Job': node_state[node]['Job']}
     
     return low_speed_nodes
 
@@ -129,7 +131,7 @@ def check_disk_usage(node_state, Disk_usage_threshold):
         if node_state[node]['State'] == 'Down':
             continue
         if float(node_state[node]['Disk'][:-1]) > Disk_usage_threshold:
-            high_usage_nodes[node] = node_state[node]['Disk']
+            high_usage_nodes[node] = {'Disk': node_state[node]['Disk'], 'Job': node_state[node]['Job']}
     
     return high_usage_nodes
 
@@ -176,12 +178,115 @@ def check_non_pbs_job(current_state, all_jobs, all_users):
                         }
                 if len(non_pbs_jobs[node][user]) == 0:
                     non_pbs_jobs[node].pop(user)
+                non_pbs_jobs[node]['pbs_user'] = pbs_user
         if len(non_pbs_jobs[node]) == 0:
             non_pbs_jobs.pop(node)
             
     return large_job_in_00, non_pbs_jobs
 
-def alert(event, data):
+def alert(event, data, send_mail):
+    mail_list = "calab-cluster-admin@googlegroups.com"
+    mail_content_file = "/tmp/mail_content"
+    local_time = time.localtime()
+    content = "%s/%s/%s %s:%s\n" %(
+        local_time.tm_year, 
+        local_time.tm_mon,
+        local_time.tm_mday,
+        local_time.tm_hour,
+        local_time.tm_min,
+    )
+    with open(mail_content_file,'w') as f:
+        if 'temp' in event:
+            content += "%-12s %-16s %-16s %-8s %8s\n" %('Node_name', 'User', 'Job_name', 'PID', 'T_' + event.split()[0])
+            for node in data:
+                counter = 0
+                for job in data[node]['Job']:
+                    if counter > 0:
+                        content += "%-12s %-16s %-16s %-8s %8s\n" %(
+                            '', 
+                            data[node]['Job'][job]['User'], 
+                            data[node]['Job'][job]['Job_name'], 
+                            job, 
+                            '',
+                        )
+                    else:
+                        content += "%-12s %-16s %-16s %-8s %8s\n" %(
+                            node, data[node]['Job'][job]['User'], 
+                            data[node]['Job'][job]['Job_name'], 
+                            job, 
+                            data[node]['T_' + event.split()[0]],
+                        )
+                    counter += 1
+
+        elif 'speed' in event:
+            content += "%-12s %6s\n" %('Node_name', event.split()[0] + '_speed')
+            for node in data:
+                content += "%-12s %6s\n" %(node, data[node][event.split()[0] + '_speed'])
+
+        elif 'usage' in event:
+            content += "%-12s %6s\n" %('Node_name', event.split()[0] + '_usage')
+            for node in data:
+                content += "%-12s %6s\n" %(node, data[node][event.split()[0]])
+
+        elif 'down' in event:
+            content += "%-12s %-16s %-16s %-8s\n" %('Node_name', 'User', 'Job', 'PID')
+            for node in data:
+                counter = 0
+                for job in data[node]['Job']:
+                    if counter > 0:
+                        content += "%-12s %-16s %-16s %-8s\n" %(
+                            '',
+                            data[node]['Job'][job]['User'],
+                            data[node]['Job'][job]['Job_name'],
+                            job,
+                        )
+                    else:
+                        content += "%-12s %-16s %-16s %-8s\n" %(
+                            node,
+                            data[node]['Job'][job]['User'],
+                            data[node]['Job'][job]['Job_name'],
+                            job,
+                        )
+                    counter += 1
+        elif "Large job" in event:
+            content += "%-16s %-8s %6s %8s %8s %s\n" %('User', 'PID', '%CPU', '%MEM', 'Time', 'Command')
+            for user in data:
+                for job in data[user]:
+                    content += "%-16s %-8s %6s %8s %8s %s\n" %(
+                        user, 
+                        job, 
+                        data[user][job]['%CPU'],
+                        data[user][job]['%MEM'],
+                        data[user][job]['Time'],
+                        data[user][job]['Command'],
+                        )
+        elif "Non-pbs job" in event:
+            content += "%-12s %-16s %-16s %-12s %s\n" %('Node', 'pbs_user', 'non-pbs_user', 'npbs_PID', 'Command')
+            for node in data:
+                pbs_user = data[node].pop('pbs_user')
+                for user in data[node]:
+                    for job in data[node][user]:
+                        content += "%-12s %-16s %-16s %-12s %s\n" %(
+                            node,
+                            pbs_user,
+                            user,
+                            job,
+                            ' '.join(data[node][user][job]['Command']),
+                        )
+        else:
+            print("Unexpected event.")
+            exit(1)
+
+        f.write(content)
+    
+    if send_mail:
+        cmd = ['mail', '-s', '"' + event + '"', mail_list, '<' , mail_content_file]
+        print(cmd)
+        print(content)
+        msg = {}
+        #rc.run_cli(cmd, msg)
+
+    #os.remove(mail_content_file)
     return 0
 
 if __name__ == '__main__':
@@ -191,46 +296,44 @@ if __name__ == '__main__':
     
     # 1. Node down
     new_down_nodes = check_node_down(current_state, previous_state)
+    if len(new_down_nodes) > 0:
+        alert("Node down", new_down_nodes, True)
+
     
     # 2. CPU high temperature
     CPU_temp_limit = 65.
     CPU_high_temp_nodes = check_CPU_temp(current_state, CPU_temp_limit)
     if len(CPU_high_temp_nodes) > 0:
-        alert("CPU high temp", CPU_high_temp_nodes)
+        alert("CPU high temp", CPU_high_temp_nodes, True)
 
-    print(CPU_high_temp_nodes)
 
     # 3. GPU high temperature
     GPU_temp_limit = 80.
     GPU_high_temp_nodes = check_GPU_temp(current_state, GPU_temp_limit)
     if len(GPU_high_temp_nodes) > 0:
-        alert("GPU high temp", GPU_high_temp_nodes)
+        alert("GPU high temp", GPU_high_temp_nodes, True)
 
-    print(GPU_high_temp_nodes)
 
     # 4. IB high temperature
-    IB_temp_limit = 105.
+    IB_temp_limit = 100.
     IB_high_temp_nodes = check_IB_temp(current_state, IB_temp_limit)
     if len(IB_high_temp_nodes) > 0:
-        alert("IB high temp", IB_high_temp_nodes)
+        alert("IB high temp", IB_high_temp_nodes, True)
 
-    print(IB_high_temp_nodes)
 
     # 5. IB speed incorrect
     IB_speed_threshold = 100.
     IB_low_speed_nodes = check_IB_speed(current_state, IB_speed_threshold)
     if len(IB_low_speed_nodes) > 0:
-        alert("IB low speed", IB_low_speed_nodes)
+        alert("IB low speed", IB_low_speed_nodes, True)
 
-    print(IB_low_speed_nodes)
 
     # 6. Disk volume uages
     Disk_usage_threshold = 80
     Disk_high_usage_nodes = check_disk_usage(current_state, Disk_usage_threshold)
     if len(Disk_high_usage_nodes) > 0:
-        alert("Disk high usage", Disk_high_usage_nodes)
+        alert("Disk high usage", Disk_high_usage_nodes, previous_state, True)
 
-    print(Disk_high_usage_nodes)
 
     # 7. Non-pbs job 
     alive_nodes = []
@@ -242,6 +345,9 @@ if __name__ == '__main__':
     all_users = {}
     rc.run_cli(['ls','/home/'], all_users)
     large_job_in_00, non_pbs_jobs = check_non_pbs_job(current_state, all_jobs, all_users['ls'])
-    print large_job_in_00
-    for node in non_pbs_jobs:
-        print(node, non_pbs_jobs[node])
+
+    if len(large_job_in_00) > 0:
+        alert("Large job in login node", large_job_in_00, True)
+
+    if len(non_pbs_jobs) > 0:
+        alert("Non-pbs job in computing nodes", non_pbs_jobs, True)
